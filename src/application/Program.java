771c57fd6e25ec66f47cc47e9d99365be34f7652
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-04 18:08:04 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a file path: ");
+		String strPath = sc.nextLine();
+		File path = new File(strPath);
+
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
+
+		sc.close();
+	}
+}
